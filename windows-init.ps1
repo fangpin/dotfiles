fe79380d47969285280f02db3b781dd52520d71b
Pin Fang <Pin.Fang@microsoft.com>
2021-03-18 14:49:04 +0800

    windows init

--- windows-init.ps1
+++ windows-init.ps1
@@ -3,3 +3,12 @@ winget install powertoys
 winget install voidtools.Everything
 winget install Tencent.wechat
 winget install Qv2ray.Qv2ray
+
+# install oh my posh 
+Install-Module posh-git -Scope CurrentUser
+Install-Module oh-my-posh -Scope CurrentUser
+Install-Module -Name PSReadLine -Scope CurrentUser -Force -SkipPublisherCheck
+echo "Import-Module posh-git" >> $profile
+echo "Import-Module oh-my-posh" >> $profile
+# install font https://github.com/microsoft/cascadia-code/releases
+# tutorial https://docs.microsoft.com/en-us/windows/terminal/tutorials/powerline-setup
